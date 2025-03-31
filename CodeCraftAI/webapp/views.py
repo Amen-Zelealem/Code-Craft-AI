49a3d96926945980d090289bc3d1b3c6fd1c431a
Amen Zelealem <amenzelealemtadese@gmail.com>
2025-03-31 10:22:12 +0300

    Added Selection Box

--- CodeCraftAI/webapp/views.py
+++ CodeCraftAI/webapp/views.py
@@ -2,4 +2,26 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, "home.html", {})
+    language_list = [
+        "c",
+        "clike",
+        "cpp",
+        "csharp",
+        "css",
+        "csv",
+        "dart",
+        "django",
+        "go",
+        "groovy",
+        "javascript",
+        "json",
+        "json5",
+        "markup",
+        "markup-templating",
+        "mongodb",
+        "nginx",
+        "php",
+        "powershell",
+    ]
+    # print(sorted(language_list))
+    return render(request, "home.html", {'language_list':language_list})
