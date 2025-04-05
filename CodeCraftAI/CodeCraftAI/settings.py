750c6a502c33ae52a1a65a69d5a9eb6fb23e2062
Amen Zelealem <amenzelealemtadese@gmail.com>
2025-04-05 09:36:43 +0300

    Completed Capstone Project

--- CodeCraftAI/CodeCraftAI/settings.py
+++ CodeCraftAI/CodeCraftAI/settings.py
@@ -10,7 +10,11 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-pi5knyqt_hphk&vriqrw!s5nwuj6^cvpq3^fm9wci*&=3jdm@a"
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG")
 
 ALLOWED_HOSTS = ["code-craft-ai.onrender.com"]
 CSRF_TRUSTED_ORIGINS = [
